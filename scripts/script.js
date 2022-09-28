a6132549460001b1616bb801f69ed996c2776184
DanteMommers <Dantemommers@gmail.com>
2022-09-28 14:40:49 +0200

    Alles

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,17 @@
+var deBody = document.querySelector("body");
+
 // Video player button 1
 var buttonWeg = document.querySelector(".PageOne section:first-of-type button");
 var homeVideo = document.querySelector(".PageOne section:first-of-type video");
+var deVideo = document.querySelector(".PageOne section:first-of-type")
+
+// Filter
+var filterButton = document.querySelector(".PageTwo section:nth-of-type(2) button");
+var filterWeg = document.querySelector(".PageTwo section:nth-of-type(1) div:nth-of-type(1) div:nth-of-type(2) > button");
+var filterKlaar = document.querySelector(".PageTwo section:nth-of-type(1) div:nth-of-type(1) div:nth-of-type(2) div:nth-of-type(6) > button:nth-of-type(2)");
 
 // Het hamburger menu
-var deBody = document.querySelector("body");
 var deNav = document.querySelector("header:nth-of-type(2) nav");
-var deVideo = document.querySelector(".PageOne section:first-of-type")
 var hamburgerMenu = document.querySelector("header:nth-of-type(2) nav button");
 var xMenu = document.querySelector("header:nth-of-type(2) nav nav ul li button");
 
@@ -18,14 +24,30 @@ function weg() {
 
 function openMenu() {
     deBody.classList.add("toonMenu")
-    // deNav.classList.add("toonMenu")
-    // deVideo.classList.add("toonMenu")
 }
 
 function sluitMenu() {
     deBody.classList.remove("toonMenu")
-    // deNav.classList.remove("toonMenu")
-    // deVideo.classList.remove("toonMenu")
+}
+
+function openFilter() {
+    deBody.classList.add("toonFilter")
+}
+
+function sluitFilter() {
+    deBody.classList.remove("toonFilter")
+}
+
+if (filterButton) {
+    filterButton.addEventListener("click", openFilter)
+}
+
+if (filterWeg) {
+    filterWeg.addEventListener("click", sluitFilter)
+}
+
+if (filterKlaar) {
+    filterKlaar.addEventListener("click", sluitFilter)
 }
 
 if (buttonWeg) {
